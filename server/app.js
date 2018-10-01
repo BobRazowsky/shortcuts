a1a30cff08f9fed4b17563ed32a44821ce7f087b
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-01 22:42:11 +0200

    go

--- server/app.js
+++ server/app.js
@@ -18,26 +18,13 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/convert', upload.single('shortcut'), function(req, res, next) {
-	//console.log('CONVERT : ', req.body);
-	// var body = req.body.name;
-	// var buf = new Buffer(body.toString('binary'),'binary');
-
- //    convert(buf, res);
  	console.log('BODY' + JSON.stringify(req.body));
  	console.log(req.file);
  	convert(req.file, res);
- 	//res.send('ok');
-	//res.send('done');
 });
 
 app.get('/shortcut', function(req, res) {
-	res.sendFile('shortcut.json', {}, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Retrieved shortcut');
-    }
-  });
+	
 });
 
 app.listen(port, function() {
@@ -46,16 +33,6 @@ app.listen(port, function() {
 
 function convert(file, res) {
 
-	//console.log(body);
-	//var buf = Buffer.from(body);
-	//
-	// fs.writeFile('shortcut.plist', buf, function (err) {
-	//   if (err) throw err;
-	//   console.log('Saved!');
-	// });
-
-	// res.download('shortcut.plist');
-
 	readBplist(file.path).then((data) => {
 
 		console.log(data);
@@ -63,15 +40,19 @@ function convert(file, res) {
 
 	 	fs.writeFile('shortcut.json', JSON.stringify(data), function (err) {
 		  if (err) throw err;
-		  res.download('shortcut.json');
+		  //res.download('shortcut.json');
+
+		  res.sendFile('shortcut.json', {}, function (err) {
+		    if (err) {
+		      console.log(err);
+		    } else {
+		      console.log('Retrieved shortcut');
+		    }
+		  });
+
 		  console.log('Saved!');
 		});
 
-		
-
-		//res.send('YOLO');
-
 	});
 
-	//res.send('OK');
 }
