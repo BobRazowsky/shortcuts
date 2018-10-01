3437b4a061109c84d98d7b138cb1c6322e59cbab
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-01 23:07:31 +0200

    go

--- app/js/viewer.js
+++ app/js/viewer.js
@@ -8,10 +8,10 @@ function init() {
 
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = () => {
-	    if (this.readyState == 4 && this.status == 200) {
+	    //if (this.readyState == 4 && this.status == 200) {
 	    	shortcut = JSON.parse(xhttp.responseText);
 	    	console.log('SHORTCUT', shortcut);
-	    }
+	    //}
 	};
 	xhttp.open("GET", "/shortcut", true);
 	xhttp.send();
