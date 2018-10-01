a9027691c7a46b73d0da16b6f514a52d64fc3dae
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-01 19:51:41 +0200

    go

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var readBplist = require('read-bplist');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
@@ -7,8 +8,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.use(bodyParser);
+
 app.post('/convert', function(req, res) {
-	console.log('CONVERT : ', req, res);
+	console.log('CONVERT : ', req.body);
 	// readBplist('path/to/your.bplist').then((data) => {
 	// 	console.log(data);
 	//   //res.send(data);
