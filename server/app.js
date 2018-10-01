28580444b1131e449249986358fef861a81469be
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-01 19:12:47 +0200

    test

---
+++ server/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send('test');
+});
+
+app.listen(port, function() {
+	console.log('Listening app on port ' + port);
+});
