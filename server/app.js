2d6e2f455b8f19dbbcf443c5108f41b93e340fb3
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-01 19:19:13 +0200

    server

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(__dirname + '/build/release/'));
+app.use(express.static(__dirname + '/build/release'));
 
 app.get('/', function(req, res) {
   //res.send('test');
