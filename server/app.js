e29ffea2c40f028448e01235e855b7287ae29958
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-01 20:41:15 +0200

    go

--- server/app.js
+++ server/app.js
@@ -35,8 +35,9 @@ app.listen(port, function() {
 function convert(body, res) {
 
 	//console.log(body);
+	var buf = new Buffer(body);
 	//
-	fs.writeFile('shortcut.plist', body, function (err) {
+	fs.writeFile('shortcut.plist', buf, function (err) {
 	  if (err) throw err;
 	  console.log('Saved!');
 	});
