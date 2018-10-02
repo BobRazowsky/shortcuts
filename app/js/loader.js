f52a117449b974689aa7e106694709c5a2d1deee
Arighyni <arighyni@wanadev.fr>
2018-10-02 11:24:46 +0200

    're'

--- app/js/loader.js
+++ app/js/loader.js
@@ -1,4 +1,5 @@
 document.getElementById('shortcut').onchange = function () {
 	console.log('bru');
+	var value = this.value.replace("C:\\fakepath\\", "");
 	document.getElementById('selectedFile').innerHTML = this.value;
 };
