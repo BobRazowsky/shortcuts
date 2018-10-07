37dfd1e3ebf2b101423b5ec7cc378b3562188324
Bob Razowsky <aymeric.righyni@gmail.com>
2018-10-07 23:49:16 +0200

    dict updates

--- app/js/viewer.js
+++ app/js/viewer.js
@@ -98,23 +98,23 @@ function displayQuestions(questions) {
 		nodeTop.classList.add('nodeTop');
 		node.appendChild(nodeTop);
 
-		// var nodeIconSrc = document.getElementById('node' + questions[i].ActionIndex.childNodes[0].childNodes[0].src);
-		// var nodeTitleTxt = document.getElementById('node' + questions[i].ActionIndex.childNodes[0].childNodes[1].innerHTML);
+		var nodeIconSrc = document.getElementById('node' + questions[i].ActionIndex).childNodes[0].childNodes[0].src;
+		var nodeTitleTxt = document.getElementById('node' + questions[i].ActionIndex).childNodes[0].childNodes[1].innerHTML;
 
 		console.log(document.getElementById('node' + questions[i].ActionIndex).childNodes, questions[i].ActionIndex);
 
-		// var nodeIcon = document.createElement('img');
-		// nodeIcon.src = nodeIconSrc;
-		// nodeIcon.alt = nodeTitleTxt;
-		// nodeIcon.classList.add('icon');
-		// nodeIcon.width = 32;
-		// nodeIcon.height = 32;
-		// nodeTop.appendChild(nodeIcon);
+		var nodeIcon = document.createElement('img');
+		nodeIcon.src = nodeIconSrc;
+		nodeIcon.alt = nodeTitleTxt;
+		nodeIcon.classList.add('icon');
+		nodeIcon.width = 32;
+		nodeIcon.height = 32;
+		nodeTop.appendChild(nodeIcon);
 
-		// var nodeTitle = document.createElement('p');
-		// nodeTitle.classList.add('nodeTitle');
-		// nodeTitle.innerHTML = nodeTitleTxt;
-		// nodeTop.appendChild(nodeTitle);
+		var nodeTitle = document.createElement('p');
+		nodeTitle.classList.add('nodeTitle');
+		nodeTitle.innerHTML = nodeTitleTxt;
+		nodeTop.appendChild(nodeTitle);
 	}
 }
 
